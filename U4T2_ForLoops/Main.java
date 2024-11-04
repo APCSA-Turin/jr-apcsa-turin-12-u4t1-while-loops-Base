c7b56137032360e437e69fb6263d71af748efbcb
Jarian-M <jarianm2@nycstudents.net>
2024-11-04 14:31:30 +0000

    fruit

---
+++ U4T2_ForLoops/Main.java
@@ -0,0 +1,12 @@
+package U4T2_ForLoops;
+
+public class Main {
+    public static void main (String[] args) {
+        int num = 100;
+        for (int count = 1; count <= num; count++) {
+          System.out.print(num + " ");
+          num = num / 2;
+        }
+           
+    }
+}
